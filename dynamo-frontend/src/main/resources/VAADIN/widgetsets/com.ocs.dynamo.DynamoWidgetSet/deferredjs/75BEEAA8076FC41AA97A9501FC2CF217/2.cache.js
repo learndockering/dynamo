f409a5f1fa3a36cebd9dd0bc2892050dcb3c73c6
Michael Westergaard <michael.westergaard@opencircle.solutions>
2018-04-11 16:56:21 +0200

    Add GanttChart to widget set

---
+++ dynamo-frontend/src/main/resources/VAADIN/widgetsets/com.ocs.dynamo.DynamoWidgetSet/deferredjs/75BEEAA8076FC41AA97A9501FC2CF217/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_ocs_dynamo_DynamoWidgetSet.runAsyncCallback2('Fmb(1706,1,hpe);_.ce=function erc(){H9b((!A9b&&(A9b=new M9b),A9b),this.a.d)};Xhe(Mm)(2);\n//# sourceURL=com.ocs.dynamo.DynamoWidgetSet-2.js\n')
